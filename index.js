82e2cb39d3ce2664ab6c1c013d0b9fc3c6adff8a
sethvincent <sethvincent@gmail.com>
2015-12-22 17:46:26 -0800

    v1.0.0

---
+++ index.js
@@ -0,0 +1,111 @@
+/**
+* Create an event emitter with namespaces
+* @name createNamespaceEmitter
+* @example
+* var emitter = require('./index')()
+*
+* emitter.on('*', function () {
+*   console.log('all events emitted', this.event)
+* })
+*
+* emitter.on('example', function () {
+*   console.log('example event emitted')
+* })
+*/
+module.exports = function createNamespaceEmitter () {
+  var emitter = { _fns: {} }
+
+  /**
+  * Emit an event. Optionally namespace the event. Separate the namespace and event with a `:`
+  * @name emit
+  * @param {String} event – the name of the event, with optional namespace
+  * @param {...*} data – data variables that will be passed as arguments to the event listener
+  * @example
+  * emitter.emit('example')
+  * emitter.emit('demo:test')
+  * emitter.emit('data', { example: true}, 'a string', 1)
+  */
+  emitter.emit = function emit (event) {
+    var args = [].slice.call(arguments, 1)
+    var namespaced = namespaces(event)
+    if (this._fns[event]) emitAll(event, this._fns[event], args)
+    if (namespaced) emitAll(event, namespaced, args)
+  }
+
+  /**
+  * Create en event listener.
+  * @name on
+  * @param {String} event
+  * @param {Function} fn
+  * @example
+  * emitter.on('example', function () {})
+  * emitter.on('demo', function () {})
+  */
+  emitter.on = function on (event, fn) {
+    if (typeof fn !== 'function') { throw new Error('callback required') }
+    (this._fns[event] = this._fns[event] || []).push(fn)
+  }
+
+  /**
+  * Create en event listener that fires once.
+  * @name once
+  * @param {String} event
+  * @param {Function} fn
+  * @example
+  * emitter.once('example', function () {})
+  * emitter.once('demo', function () {})
+  */
+  emitter.once = function once (event, fn) {
+    function one () {
+      fn.apply(this, arguments)
+      emitter.off(event, one)
+    }
+    this.on(event, one)
+  }
+
+  /**
+  * Stop listening to an event. Stop all listeners on an event by only passing the event name. Stop a single listener by passing that event handler as a callback.
+  * You must be explicit about what will be unsubscribed: `emitter.off('demo')` will unsubscribe an `emitter.on('demo')` listener, 
+  * `emitter.off('demo:example')` will unsubscribe an `emitter.on('demo:example')` listener
+  * @name off
+  * @param {String} event
+  * @param {Function} [fn] – the specific handler
+  * @example
+  * emitter.off('example')
+  * emitter.off('demo', function () {})
+  */
+  emitter.off = function off (event, fn) {
+    var keep = []
+
+    if (event && fn) {
+      for (var i = 0; i < this._fns.length; i++) {
+        if (this._fns[i] !== fn) {
+          keep.push(this._fns[i])
+        }
+      }
+    }
+
+    keep.length ? this._fns[event] = keep : delete this._fns[event]
+  }
+
+  function namespaces (e) {
+    var out = []
+    var args = e.split(':')
+    var fns = emitter._fns
+    Object.keys(fns).forEach(function (key) {
+      if (key === '*') out = out.concat(fns[key])
+      if (args.length === 2 && args[0] === key) out = out.concat(fns[key])
+    })
+    return out
+  }
+
+  function emitAll (e, fns, args) {
+    for (var i = 0; i < fns.length; i++) {
+      if (!fns[i]) break
+      fns[i].event = e
+      fns[i].apply(fns[i], args)
+    }
+  }
+
+  return emitter
+}
---
+++ test.js
@@ -0,0 +1,165 @@
+var test = require('tape')
+var createEmitter = require('./index')
+
+test('create an event listener', function (t) {
+  var emitter = createEmitter()
+  emitter.on('example', function () {})
+  t.ok(emitter._fns, 'emitter should have a `_fns` object')
+  t.ok(emitter._fns['example'], 'emitter `_fns` object should have an `example` property')
+  t.end()
+})
+
+test('emit to an event listener', function (t) {
+  var emitter = createEmitter()
+
+  emitter.on('example', function (data) {
+    t.ok(data.example)
+    t.end()
+  })
+
+  emitter.emit('example', { example: true })
+})
+
+test('emit to an event listener with a namespace', function (t) {
+  var emitter = createEmitter()
+
+  emitter.on('test', function (data) {
+    t.ok(data.example)
+    t.end()
+  })
+
+  emitter.emit('test:example', { example: true })
+})
+
+test('remove all listeners of an event', function (t) {
+  var emitter = createEmitter()
+  var count = 0
+
+  emitter.on('example', function (data) {
+    t.ok(data.example)
+    count++
+  })
+
+  emitter.on('example', function (data) {
+    t.ok(data.example)
+    count++
+  })
+
+  emitter.emit('example', { example: true })
+  emitter.off('example')
+  emitter.emit('example', { example: true })
+  t.equal(count, 2)
+  t.end()
+})
+
+test('listen to all events', function (t) {
+  var emitter = createEmitter()
+  var count = 0
+
+  emitter.on('*', function () {
+    count++
+    if (count === 3) {
+      t.end()
+    }
+  })
+
+  emitter.emit('example')
+  emitter.emit('test')
+  emitter.emit('ok')
+})
+
+test('listen to an event only once', function (t) {
+  var emitter = createEmitter()
+  var count = 0
+
+  emitter.once('example', function () {
+    count++
+  })
+
+  emitter.emit('example')
+  emitter.emit('example')
+  t.equal(count, 1)
+  t.end()
+})
+
+test('passes all arguments to event listener', function (t) {
+  var emitter = createEmitter()
+
+  emitter.on('example', function (a, b, c, d, e, f) {
+    t.ok(a)
+    t.ok(b)
+    t.ok(c)
+    t.ok(d)
+    t.ok(e)
+    t.ok(f)
+  })
+
+  emitter.emit('example', 1, 2, 3, 4, 5, 6)
+  t.end()
+})
+
+test('unsubscribe event listeners', function (t) {
+  var emitter = createEmitter()
+  function fn () { t.fail('should not be called') }
+  emitter.on('example', fn)
+  function fn2 () { t.fail('should not be called') }
+  emitter.on('example', fn2)
+  emitter.off('example')
+  t.end()
+})
+
+test('unsubscribe single event handler', function (t) {
+  var emitter = createEmitter()
+  emitter.on('example', function () {})
+  function fn () { t.fail('should not be called') }
+  emitter.on('example', fn)
+  emitter.off('example', fn)
+  t.end()
+})
+
+test('unsubscribe event handler that has been subscribed twice', function (t) {
+  var emitter = createEmitter()
+  function fn () { t.fail('should not be called') }
+  emitter.on('example', fn)
+  emitter.on('example', fn)
+  emitter.off('example')
+  t.end()
+})
+
+test('emit event when unsubscribed in event callback', function (t) {
+  var emitter = createEmitter()
+
+  emitter.on('example', function () {
+    emitter.off('example')
+    t.ok('emitted once')
+  })
+
+  emitter.off('example')
+  t.end()
+})
+
+test('callback is required for on method', function (t) {
+  var emitter = createEmitter()
+
+  try {
+    emitter.on('example')
+  } catch (e) {
+    t.ok(e)
+  }
+
+  t.end()
+})
+
+test('handler only called once if namespaced', function (t) {
+  var emitter = createEmitter()
+  var count = 0
+
+  emitter.on('example:first', function (data) {
+    t.ok(data.example)
+    count++
+  })
+
+  emitter.emit('example:first', { example: true })
+  t.equal(count, 1)
+  t.end()
+})
